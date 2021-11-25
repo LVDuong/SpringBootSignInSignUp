fd29d83aa9075ab37308fe7997ce6ef9daefd978
Duong Le <duong.le@zamo.io>
2021-11-25 08:03:37 +0700

    Test probot

--- src/main/java/com/javaspring/signinsignup/SpringBootSignInSignUpApplication.java
+++ src/main/java/com/javaspring/signinsignup/SpringBootSignInSignUpApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootSignInSignUpApplication {
 	// AAAAAAAAAAAAAAAAAAA
-
+	// BBBBBBBBBBBBBBBB
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSignInSignUpApplication.class, args);
 	}
