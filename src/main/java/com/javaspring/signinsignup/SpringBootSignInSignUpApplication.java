8b35cbc6fde6dde16178402f4ca3da810707e441
Duong Le <duong.le@zamo.io>
2021-11-25 09:43:54 +0700

    Test Release Drafter v2

--- src/main/java/com/javaspring/signinsignup/SpringBootSignInSignUpApplication.java
+++ src/main/java/com/javaspring/signinsignup/SpringBootSignInSignUpApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootSignInSignUpApplication {
-	// Release Drafter merge
+	// Release Drafter v2
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSignInSignUpApplication.class, args);
 	}
